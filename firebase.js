495c578eae4dc51fbe9716b1f9cfd873306cc6c5
Shekhar Rana <shekharrana05@gmail.com>
2022-01-10 14:32:48 +0530

    js changes

---
+++ firebase.js
@@ -0,0 +1,125 @@
+var admin = require("firebase-admin"); 
+var serviceAccount = require("./bin/nokia-firebase-sdk.json"); 
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const registerToken = async (token, topic) => {
+  try {
+    if (token) {
+      admin.messaging().subscribeToTopic(token, topic)
+        .then((response) => {
+          // See the MessagingTopicManagementResponse reference documentation
+          // for the contents of response.
+          console.log('Successfully subscribed to topic: ------- ', response);
+          return 'success'
+        })
+        .catch((error) => {
+          console.log('Error subscribing to topic:', error);
+          return error
+        });
+    }
+    else {
+      return 'error'
+    }
+  }
+  catch (error) {
+    return error
+  }
+};
+
+const sendMessageToTopic = async (topic, title, body) => {
+  try {
+    if (topic) { 
+      const payload = {
+        notification: {
+          title: title,
+          body: body
+        }
+      };
+
+      admin.messaging().sendToTopic(topic, payload)
+        .then((response) => {
+          console.log('Successfully send message to topic:', response);
+          return 'success'
+        })
+        .catch((error) => {
+          console.log('error  send message to topic 1:', error);
+          return error
+        });
+    }
+    else {
+      console.log('error  send message to topic 2:');
+      return 'topic not fount'
+    }
+  }
+  catch (error) {
+    console.log('error  send message to topic 3:', error);
+    return error
+  }
+};
+
+const sendMessageToDevice = async (tokens, title, body) => {
+  try {
+    console.log('-------sendMessageToTopic-----------------',tokens);
+    if (tokens) { 
+      const payload = {
+        notification: {
+          title: title,
+          body: body
+        }
+      }; 
+      admin.messaging().sendToDevice(tokens,payload)
+        .then((response) => {
+          console.log('Successfully send message to topic:', response);
+          return 'success'
+        })
+        .catch((error) => {
+          console.log('error  send message to topic 1:', error);
+          return error
+        });
+    }
+    else {
+      console.log('error  send message to topic 2:');
+      return 'topic not fount'
+    }
+  }
+  catch (error) {
+    console.log('error  send message to topic 3:', error);
+    return error
+  }
+};
+const sendMessageToTopicFromAPI = async (title, body, _topic) => {
+  try {
+    if (title && body) {
+
+      const payload = {
+        notification: {
+          title: title,
+          body: body
+        }
+      };
+
+      const topic = _topic; 
+
+      admin.messaging().sendToTopic(topic, payload)
+        .then((response) => {
+          console.log('Successfully subscribed to topic:', response);
+        })
+        .catch((error) => {
+          console.log('Error subscribing to topic:', error);
+        });
+    }
+    else {
+
+    }
+  }
+  catch (error) {
+
+  }
+};
+
+module.exports.registerToken = registerToken;
+module.exports.sendMessageToTopic = sendMessageToTopic;
+module.exports.sendMessageToTopicFromAPI = sendMessageToTopicFromAPI;
+module.exports.sendMessageToDevice = sendMessageToDevice;
--- job.js
+++ job.js
@@ -7,70 +7,208 @@
  *
  */
 
- const mysql = require('mysql2');
- //const razorPay = require('./util/razor.pay.wrapper');
- //const logger = require('./util/logger');
- const util = require('./util');
- const cronJob = require('node-cron');
- 
- //util.initApp();
- 
- const connection = mysql.createConnection({
-   host: process.env.HOST,
-   user: process.env.DBUSER,
-   password: process.env.PASSWORD,
-   database: process.env.DATABASE,
-   multipleStatements: true,
- });
- 
- 
- connection.connect((err) => {
-   if (err) throw err;
- });
- 
- // // DB Connected
- 
- const dateAdd = (date, interval, units) => {
-   let ret = new Date(date); // don't change original date
-   const checkRollover = () => { if (ret.getDate() !== date.getDate()) ret.setDate(0); };
- 
-   switch (interval.toLowerCase()) {
-     case 'year': ret.setFullYear(ret.getFullYear() + units); checkRollover(); break;
-     case 'quarter': ret.setMonth(ret.getMonth() + 3 * units); checkRollover(); break;
-     case 'month': ret.setMonth(ret.getMonth() + units); checkRollover(); break;
-     case 'week': ret.setDate(ret.getDate() + 7 * units); break;
-     case 'day': ret.setDate(ret.getDate() + units); break;
-     case 'hour': ret.setTime(ret.getTime() + units * 3600000); break;
-     case 'minute': ret.setTime(ret.getTime() + units * 60000); break;
-     case 'second': ret.setTime(ret.getTime() + units * 1000); break;
-     default: ret = undefined; break;
-   }
-   return ret;
- };
- 
- 
- // Schedule a task to run every minute.
- cronJob.schedule('* * * * *', () => { console.log("Task is running every minute :", new Date()) });
- 
- 
+const mysql = require('mysql2');
+//const razorPay = require('./util/razor.pay.wrapper');
+//const logger = require('./util/logger');
+const util = require('./util');
+const cronJob = require('node-cron');
+const dal = require('./dal')
+const { db } = require('./models');
+const firebase = require("./firebase");
+const _ = require('lodash')
+//util.initApp();
+
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.DBUSER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  multipleStatements: true,
+});
+
+
+connection.connect((err) => {
+  if (err) throw err;
+});
+
+// // DB Connected
+
+const dateAdd = (date, interval, units) => {
+  let ret = new Date(date); // don't change original date
+  const checkRollover = () => { if (ret.getDate() !== date.getDate()) ret.setDate(0); };
+
+  switch (interval.toLowerCase()) {
+    case 'year': ret.setFullYear(ret.getFullYear() + units); checkRollover(); break;
+    case 'quarter': ret.setMonth(ret.getMonth() + 3 * units); checkRollover(); break;
+    case 'month': ret.setMonth(ret.getMonth() + units); checkRollover(); break;
+    case 'week': ret.setDate(ret.getDate() + 7 * units); break;
+    case 'day': ret.setDate(ret.getDate() + units); break;
+    case 'hour': ret.setTime(ret.getTime() + units * 3600000); break;
+    case 'minute': ret.setTime(ret.getTime() + units * 60000); break;
+    case 'second': ret.setTime(ret.getTime() + units * 1000); break;
+    default: ret = undefined; break;
+  }
+  return ret;
+};
+
+
+// Schedule a task to run every minute.
+cronJob.schedule('* * * * *', () => { console.log("Task is running every minute :", new Date()) });
+cronJob.schedule('*/03 * * * *', () => {
+  console.log("Task is running every two minute :", new Date())
+  try {
+    connection.query("CALL asp_nk_cm_get_notification_master_details('','','','');", (error, results, fields) => {
+      if (error) {
+        console.error(error.message);
+      }
+      else if (results) {
+        //console.log("results : ", results);       
+        if (results && results.length > 0) {
+          for (var item of results[0]) {
+            console.log(item);
+            //item.escalationFunctionName(item);
+            //console.log("item.escalationFunctionName : ", item.escalationFunctionName);
+            let notificatioCode = item && item.notificationCode;
+            switch (notificatioCode) {
+              case "INC":
+                internetNotConnectedAlarm(item);
+              //  break;
+              // default:
+              //  break;
+            }
+          }
+        }
+
+      }
+    });
+  }
+  catch (error) {
+    console.log("error : ", error);
+  }
+});
+
+
+const internetNotConnectedAlarm = async (notificationDetails) => {
+  //
+  try {
+    let notificationCode = notificationDetails && notificationDetails.notificationCode ? notificationDetails.notificationCode : 'INC';
+    let message = notificationDetails && notificationDetails.message ? notificationDetails.message : '';
+    let title = notificationDetails && notificationDetails.title ? notificationDetails.title : '';
+
+    const promise = new Promise((resolve, reject) => {
+      db.sequelize.query('call asp_nk_get_tmc_internet_not_conneted_details(:p_notificationCode)', {
+        replacements: {
+          p_notificationCode: notificationCode
+        }
+      }).then((result) => {
+        resolve(result);
+        console.log("--------internetNotConnectedAlarm------", result)
+        let macAddressArray = [];
+        let receiverIdArray = [];
+        const mappedArray =   result && result.length > 0 &&   _.map(result,'macAddress')
+        const mappedArray2 =   result && result.length > 0 &&   _.map(result,'receiverId')
+        result && result.length > 0 && result.forEach(element => {
+          macAddressArray.push(element.macAddress);
+          receiverIdArray.push(element.receiverId);
+        }
+        ); 
+       // const mappedArray = _.map(macAddressArray)
+        const uniqueadta = macAddressArray && macAddressArray.length > 0 && _.uniq(macAddressArray)
+        const uniqueReceiverId = receiverIdArray && receiverIdArray.length > 0 && _.uniq(receiverIdArray)
+
+        uniqueadta && uniqueadta.forEach(reminder => {
+          SaveNotificationDetails(notificationDetails, reminder);
+        });
+
+        if (uniqueReceiverId) {
+          pushFCMNotificationDetails(uniqueReceiverId, title, message)
+        }
+
+      }).catch((error) => {
+        console.log('error -------------', error);
+        reject(error);
+      });
+    });
+
+    await promise;
+
+    //const capturedResult = await razorPay.capture(id, amount);
+  }
+  catch (error) {
+    console.log("error : ", error);
+  }
+};
+
+const SaveNotificationDetails = async (notificationDetails, macAddress) => {
+  //
+  try {
+    console.log("notificationDetails 1 3434----------------: ", notificationDetails);
+
+    let message = notificationDetails && notificationDetails.message ? notificationDetails.message : '';
+    let title = notificationDetails && notificationDetails.title ? notificationDetails.title : '';
+    let notificationCode = notificationDetails && notificationDetails.notificationCode ? notificationDetails.notificationCode : '';
+    let towerMonitoringNotificationDetailid = dal.uuid(db.towerMonitoringNotificationDetails.name);
+
+    const promise = new Promise((resolve, reject) => {
+
+      db.sequelize.query('call asp_nk_save_tower_monitoring_notification_details(:p_tower_monitoring_notification_detail_id,:p_mac_address,:p_notification_code,:p_title,:p_message,:p_data_time,:p_created_by)', {
+        replacements: {
+          p_tower_monitoring_notification_detail_id: towerMonitoringNotificationDetailid,
+          p_mac_address: macAddress,
+          p_notification_code: notificationCode,
+          p_title: title,
+          p_message: message,
+          p_data_time: new Date(),
+          p_created_by: '-1',
+        }
+      }).then((result) => {
+        resolve(result);
+        console.log('----------SaveNotificationDetails----------', result)
+      }).catch((error) => {
+        reject(error);
+      });
+    });
+    await promise;
+  }
+  catch (error) {
+    console.log("error : ", error);
+  }
+};
+
+const pushFCMNotificationDetails = async (receiverIds, title, message) => {
+  try {
+    console.log("-------------pushFCMNotificationDetails----receiverIds-----------", receiverIds)
+    const promise = new Promise((resolve, reject) => {
+      firebase.sendMessageToDevice(receiverIds, title, message).then((result) => {
+        resolve(result);
+      }).catch((error) => {
+        reject(error);
+      });
+    });
+    await promise;
+  }
+  catch (error) {
+    console.log("error : ", error);
+  }
+};
  //-----------------Final Audit Auto Submission Job----------------------------
-  
+
  //-----------------------End------------------------------------------------
- 
- 
- 
+
+
+
  // /**
  //  * It will take the payment id, and capture it
  //  */
  // const capturePayment = async (id, amount) => {
  //   // when we capture a payment, we need to make sure that the status is rolled back to the DB
- 
+
  //   // if we an error in this method, let's just send an email
- 
+
  //   // To-do, let's try to make it a multiple statement instead of one by one
  //   //
  //   const capturedResult = await razorPay.capture(id, amount);
- 
+
  //   // once captured, let's store it in DB
  //   const promise = new Promise((resolve, reject) => {
  //     db.sequelize.query('call set_razory_pay_status(:razorPayId, :razorPayStatus, :capturedAt);', {
@@ -81,15 +219,15 @@
  //       reject(error);
  //     });
  //   });
- 
+
  //   await promise;
  // };
- 
- 
+
+
  // /**
  //  * Capturing payment on Razor Pay
  //  */
- 
+
  // const autoCaptureAll = async () => {
  //   try {
  //     //
@@ -98,17 +236,17 @@
  //          * We will run this job every 30 minutes to capture the payment.
  //          * This will be handy if a payment was not captured initially while completing the payment
  //          */
- 
+
  //     const paymentTo = Number(new Date());
  //     const paymentFrom = Number(dateAdd(paymentTo, 'minute', -30));
- 
+
  //     const payments = await razorPay.getPayments(paymentFrom, paymentTo);
- 
+
  //     /** To-do: we should not hit DB multiple times, so we will collect all the ids and do only 3-4 requests */
- 
- 
+
+
  //     // let's loop through all the payments and find if there are any items to authorize.
- 
+
  //     payments.forEach(async (payment) => {
  //       try {
  //         if (payment.status === 'authorized' && payment.entity === 'payment') {
@@ -124,7 +262,7 @@
  //               reject(error);
  //             });
  //           });
- 
+
  //           await promise;
  //         }
  //       } catch (error) {
@@ -147,8 +285,7 @@
  //     }
  //   }
  // };
- 
- 
- 
- 
- 
+
+
+
+
